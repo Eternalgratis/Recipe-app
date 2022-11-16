286ac2af8a4736b48ad3f8bd16a14ad3ea4f5c38
unknown <tyaps002@gmail,com>
2022-11-16 16:13:54 +0100

    Implement relationship for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+    belongs_to :user, foreign_key: "user_id"
+    has_many :recipe_food, foreign_key: "food_id"
+    has_many :Inventory_food, foreign_key: "food_id"
 end
